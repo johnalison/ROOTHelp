dcd49c652131bec7a71f7083dfe097d775c28a3d
John Alison <johnda102@gmail.com>
2019-08-30 10:17:25 -0400

    Update with FancyStyle

---
+++ FancyROOTStyle.h
@@ -0,0 +1,21 @@
+//
+//   @file    FancyStyle.h         
+//   
+//           FancyROOTStyle,  based on a style file from ATLAS,  which was based on a style file from BaBar
+//
+//
+//   @author J.Alison
+//   @author M.Sutton
+// 
+//
+
+#ifndef  __FANCYSTYLE_H
+#define __FANCYSTYLE_H
+
+#include "TStyle.h"
+
+void SetFancyStyle();
+
+TStyle* FancyStyle(); 
+
+#endif // __FANCYSTYLE_H
---
+++ FancyROOTStyle.py
@@ -0,0 +1,6 @@
+#from ROOT import 
+import ROOT
+ROOT.gROOT.SetMacroPath(ROOT.gROOT.GetMacroPath()+":../../ROOTHelp")
+ROOT.gROOT.LoadMacro("FancyROOTStyle.C")
+ROOT.SetFancyStyle()
+
