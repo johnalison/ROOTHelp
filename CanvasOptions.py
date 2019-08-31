7d7fe1f68c66e3d299c66806f1581ff57c828c0e
John Alison <johnda102@gmail.com>
2019-08-31 13:14:59 -0400

    Update CanvasOptions

--- CanvasOptions.py
+++ CanvasOptions.py
@@ -3,7 +3,7 @@
 #
 import ROOT
 
-import ROOTCoreOLD
+import ROOTHelp
 
 class CanvasOptions:
     """Class for configuring canvas properties.""" 
@@ -21,10 +21,10 @@ class CanvasOptions:
         kw.setdefault('grid_y',        0)
         kw.setdefault('tick_x',        1)
         kw.setdefault('tick_y',        1)
-        kw.setdefault('left_margin',   ROOTCoreOLD.default)
-        kw.setdefault('right_margin',  ROOTCoreOLD.default)
-        kw.setdefault('top_margin',    ROOTCoreOLD.default)
-        kw.setdefault('bottom_margin', ROOTCoreOLD.default)
+        kw.setdefault('left_margin',   ROOTHelp.default)
+        kw.setdefault('right_margin',  ROOTHelp.default)
+        kw.setdefault('top_margin',    ROOTHelp.default)
+        kw.setdefault('bottom_margin', ROOTHelp.default)
         for k,v in kw.iteritems():
             setattr(self, k, v)
 
@@ -37,21 +37,21 @@ class CanvasOptions:
         c.SetLogx(self.log_x)
         c.SetLogy(self.log_y)
         c.SetLogz(self.log_z)
-        if not self.grid_x is ROOTCoreOLD.default:
+        if not self.grid_x is ROOTHelp.default:
             c.SetGridx(self.grid_x)
-        if not self.grid_y is ROOTCoreOLD.default:
+        if not self.grid_y is ROOTHelp.default:
             c.SetGridy(self.grid_y)
-        if not self.tick_x is ROOTCoreOLD.default:
+        if not self.tick_x is ROOTHelp.default:
             c.SetTickx(self.tick_x)
-        if not self.tick_y is ROOTCoreOLD.default:
+        if not self.tick_y is ROOTHelp.default:
             c.SetTicky(self.tick_y)
-        if not self.left_margin is ROOTCoreOLD.default:
+        if not self.left_margin is ROOTHelp.default:
             c.SetLeftMargin(self.left_margin)
-        if not self.right_margin is ROOTCoreOLD.default:
+        if not self.right_margin is ROOTHelp.default:
             c.SetRightMargin(self.right_margin)
-        if not self.top_margin is ROOTCoreOLD.default:
+        if not self.top_margin is ROOTHelp.default:
             c.SetTopMargin(self.top_margin)
-        if not self.bottom_margin is ROOTCoreOLD.default:
+        if not self.bottom_margin is ROOTHelp.default:
             c.SetBottomMargin(self.bottom_margin)
         c.SetBorderSize(0)
         c.SetBorderMode(0)
@@ -60,8 +60,8 @@ class CanvasOptions:
     #
     #  Create the specifed canvas
     # 
-    def create(self, name, title=ROOTCoreOLD.default):
-        if title is ROOTCoreOLD.default:
+    def create(self, name, title=ROOTHelp.default):
+        if title is ROOTHelp.default:
             title = name
         c = ROOT.TCanvas(name, name, 200, 10, self.width, self.height)
         self.configure(c)
