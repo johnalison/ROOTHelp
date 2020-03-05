8b50d426aa2812cd2f56fed9317197ba7ef6bccf
John Alison <johnalison@cmu.edu>
2020-03-05 11:05:03 -0600

    Add protection for emptyon python path

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 # Absolute path to this script. 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [ -z "$PYTHONPATH" ]
+then
+    export PYTHONPATH=$HOME
+fi
+
 
 PYTHONPATH=$PYTHONPATH:$THISDIR:$THISDIR/../:$THISDIR/iPlot:$THISDIR/iPlot/models
-
-
 
 
 alias iPlot='python -i ${THISDIR}/iPlot/iPlot.py'
